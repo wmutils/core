4d8d7118beb336f0bf9e18cdbcb415a49e249b7a
z3bra <willy@mailoo.org>
2014-12-09 12:22:58 +0100

    killw: util.c refactoring

--- killw.c
+++ killw.c
@@ -3,37 +3,22 @@
 #include <err.h>
 #include <xcb/xcb.h>
 
+#include "util.h"
+
 static xcb_connection_t *conn;
-
-static void xcbinit(void);
-static void cleanup(void);
-
-static void
-xcbinit(void)
-{
-	conn = xcb_connect(NULL, NULL);
-	if (xcb_connection_has_error(conn))
-		errx(1, "xcb_connect");
-}
-
-static void
-cleanup(void)
-{
-	if (conn != NULL)
-		xcb_disconnect(conn);
-}
 
 int
 main(int argc, char **argv)
 {
-	atexit(cleanup);
-	xcbinit();
+	init_xcb(&conn);
 
 	/* assume remaining arguments are windows */
 	while (*argv)
 		xcb_kill_client(conn, strtoul(*argv++, NULL, 16));
 
 	xcb_flush(conn);
+
+	kill_xcb(&conn);
 
 	return 0;
 }
