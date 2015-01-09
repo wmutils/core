62f0fe421dd4fd37a767309415fc7ea2d0556287
z3bra <willy@mailoo.org>
2015-01-09 18:50:35 +0100

    Fixed wattr usage and manpage

--- wattr.c
+++ wattr.c
@@ -27,7 +27,7 @@ enum {
 static void
 usage(char *name)
 {
-	fprintf(stderr, "usage: %s [-h] [bmiwhxy] <wid>\n", name);
+	fprintf(stderr, "usage: %s [-h] [bmiowhxy] <wid>\n", name);
 	exit(1);
 }
 
@@ -79,20 +79,30 @@ main(int argc, char **argv)
 
 		for (i=0; i<strlen(argv[1]); i++) {
 			switch (argv[1][i]) {
-				case 'i': printf("0x%08x", w);
-					  break;
-				case 'b': printf("%d", get_attribute(w, ATTR_B));
-					  break;
-				case 'h': printf("%d", get_attribute(w, ATTR_H));
-					  break;
-				case 'x': printf("%d", get_attribute(w, ATTR_X));
-					  break;
-				case 'y': printf("%d", get_attribute(w, ATTR_Y));
-					  break;
-				case 'w': printf("%d", get_attribute(w, ATTR_W));
-					  break;
-				case 'o': ret = ignore(conn, w) ? 0 : 1; goto end;
-				case 'm': ret = mapped(conn, w) ? 0 : 1; goto end;
+				case 'i':
+					printf("0x%08x", w);
+					break;
+				case 'b':
+					printf("%d", get_attribute(w, ATTR_B));
+					break;
+				case 'h': 
+					printf("%d", get_attribute(w, ATTR_H));
+					break;
+				case 'x':
+					printf("%d", get_attribute(w, ATTR_X));
+					break;
+				case 'y':
+					printf("%d", get_attribute(w, ATTR_Y));
+					break;
+				case 'w':
+					printf("%d", get_attribute(w, ATTR_W));
+					break;
+				case 'o':
+					ret = ignore(conn, w) ? 0 : 1;
+					goto end;
+				case 'm':
+					ret = mapped(conn, w) ? 0 : 1;
+					goto end;
 				default: kill_xcb(&conn); usage(argv[0]);
 			}
 			/* add a space if more attribute come after */
