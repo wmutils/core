bd9cebbcd2292149af8f5bc3700e50253b2c177c
dcat <dcat@iotek.org>
2015-11-22 03:20:06 +0100

    wm_set_override()

--- ignw.c
+++ ignw.c
@@ -12,22 +12,12 @@ xcb_connection_t *conn;
 xcb_screen_t     *scrn;
 
 static void usage(char *);
-static void set_override(xcb_window_t, int);
 
 static void
 usage(char *name)
 {
 	fprintf(stderr, "usage: %s [-sr] <wid> [wid..]\n", name);
 	exit(1);
-}
-
-static void
-set_override(xcb_window_t w, int or)
-{
-	uint32_t mask = XCB_CW_OVERRIDE_REDIRECT;
-	uint32_t val[] = { or };
-
-	xcb_change_window_attributes(conn, w, mask, val);
 }
 
 int
@@ -45,7 +35,7 @@ main(int argc, char **argv)
 	wm_init_xcb(&conn);
 
 	while (*argv)
-		set_override(strtoul(*argv++, NULL, 16), setflag);
+		wm_set_override(strtoul(*argv++, NULL, 16), setflag);
 
 	xcb_flush(conn);
 
