977535143f57d53ea186df7f2a94297139a41043
z3bra <willyatmailoodotorg>
2016-03-15 13:24:49 +0100

    killw: kill window instead of client
    Thanks to @clehner, killw now destroys the given window rather than
    killing its parent application. This is the default behavior.
    
    The old behavior (kill the parent) can be obtained with the -p flag.

--- killw.c
+++ killw.c
@@ -5,6 +5,7 @@
 #include <err.h>
 #include <xcb/xcb.h>
 
+#include "arg.h"
 #include "util.h"
 
 static xcb_connection_t *conn;
@@ -14,24 +15,38 @@ static void usage(char *);
 static void
 usage (char *name)
 {
-	fprintf(stderr, "usage: %s <wid> [wid...]\n", name);
+	fprintf(stderr, "usage: %s [-p] <wid> [wid...]\n", name);
 	exit(1);
 }
 
 int
 main(int argc, char **argv)
 {
+	int parent = 0;
+	char *argv0;
+
 	if (argc < 2)
 		usage(argv[0]);
+
+	ARGBEGIN{
+	case 'p': parent=1; break;
+	default: usage(argv0);
+	}ARGEND;
 
 	init_xcb(&conn);
 
 	/* assume remaining arguments are windows */
-	while (*argv)
-		xcb_kill_client(conn, strtoul(*argv++, NULL, 16));
+	while (*argv) {
+		if (parent) {
+			/* kills the client whose WID belongs to */
+			xcb_kill_client(conn, strtoul(*argv++, NULL, 16));
+		} else {
+			/* destroy the given window and all its children */
+			xcb_destroy_window(conn, strtoul(*argv++, NULL, 16));
+		}
+	}
 
 	xcb_flush(conn);
-
 	kill_xcb(&conn);
 
 	return 0;
